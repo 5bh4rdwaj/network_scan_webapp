08423ea9bc434640835ce49d59d23e9b2c54646f
Samvat Bhardwaj <100153846+5bh4rdwaj@users.noreply.github.com>
2023-12-22 17:16:10 +0530

    Update firesafe.sh

--- firesafe.sh
+++ firesafe.sh
@@ -1,2 +1,10 @@
 # checks network integrity
- 
+echo "initiating redition operation: FIRE-SAFE"; 
+sleep 3;
+echo "choose level (critical/moderate/routine)?";
+read ans1;
+if [ $ans1 == "critical" ]
+ echo "threat perception level increased";
+ else 
+ echo "have a great day ahead. In the name of the President";
+ fi 
